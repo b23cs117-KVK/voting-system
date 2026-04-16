002bb8f1c9d00c604b0b8a47dd73d98439db5dd4
K.VARNITHAN <b23cs117@kitsw.ac.in>
2026-04-16 18:29:13 +0530

    Cleanup: Remove diagnostic logs after successful OTP implementation

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -16,7 +16,6 @@ const generateToken = (userId) => {
 // @access  Public
 const sendOTP = async (req, res) => {
   const { email } = req.body;
-  console.log('>>> INCOMING OTP REQUEST FOR:', email);
 
   try {
     const userExists = await User.findOne({ email });
